2b3b22d0cdf1edbebb35cf4f407dd8bd57b79a42
Kelly Young <kelpopko@gmail.com>
2023-08-08 20:05:31 -0500

    Add guessing feature with comparison and scoring

--- lib/mm.rb
+++ lib/mm.rb
@@ -33,44 +33,27 @@ class CountTurns  # rename or break up this class; it's a bit hodgepodge
   def assess_the_payers_attempt_to_break_the_code
     best = 0
     next_best = 0
-    # check most specific
-    for place in (0...4)
-      binding.pry
+    rating = {}
+    for place in (0...4)      
       if @guessed_code[place] == @encoded_colors[place]
-        # best - most specific color code
         best += 1
       elsif
         @encoded_colors.any?(@guessed_code[place])
         next_best += 1
-        # next best - the general color peg
       else
-        # no peg; I don't think there's a peg for this one
+        next
       end
-
-      @best = KeyPegs.new(best).fully_correct_qty
-      @next_best = KeyPegs.new(next_best).partly_correct_qty
-      { best: "#{@best}", next_best: "#{@next_best}" }
     end
-    binding.pry
+
+    rating = KeyPegs.new(best, next_best)
+    score = { best: "#{rating.fully_correct_qty}", next_best: "#{rating.partly_correct_qty}" }
+    puts "you guessed #{@guessed_code}; code was #{@encoded_colors}" # temp
+    display_rating(score)
   end
 
-
-  def encoded_color_1
-    @encoded_color_1 ||= @encoded_colors[0]
+  def display_rating(rating)
+    puts rating.to_s
   end
-
-  def encoded_color_2
-    @encoded_color_2 ||= @encoded_colors[1]
-  end
-
-  def encoded_color_3
-    @encoded_color_3 ||= @encoded_colors[2]
-  end
-
-  def encoded_color_4
-    @encoded_color_4 ||= @encoded_colors[3]
-  end
-
 end
 
 class KeyPegs
@@ -91,65 +74,12 @@ end
 class SetCode
   COLOR_BANK = [:red, :orange, :yellow, :green, :blue, :indigo, :violet]
 
-  # hold off for when a user sets the code
-  # def initialize(color_1, color_2, color_3, color_4)
-  #   @color_1 = color_1
-  #   @color_2 = color_2
-  #   @color_3 = color_3
-  #   @color_4 = color_4
-  # end
-
-  # def set_code
-  #   # computer, later the user chooses colors
-  # end
-
-  # def color_1
-  #   @color_1
-  # end
-
-  # def color_2
-  #   @color_2
-  # end
-
-  # def color_3
-  #   @color_3
-  # end
-
-  # def color_4
-  #   @color_4
-  # end
-
   def self.all_colors
-    # [@color_1, @color_2, @color_3, @color_4]
     COLOR_BANK.shuffle.first(4)
   end
 end
 
 class GuessCode
- 
-  # def initialize(color_1, color_2, color_3, color_4)
-  #   @color_1 = color_1
-  #   @color_2 = color_2
-  #   @color_3 = color_3
-  #   @color_4 = color_4
-  # end
-
-  # def color_1
-  #   @color_1
-  # end
-
-  # def color_2
-  #   @color_2
-  # end
-
-  # def color_3
-  #   @color_3
-  # end
-
-  # def color_4
-  #   @color_4
-  # end
-
   def self.player_attempts_to_break_the_code
     puts "pick 4 colors"
     counter = 0
@@ -163,7 +93,4 @@ class GuessCode
     colors
   end
 
-  # def all_colors
-  #   [@color_1, @color_2, @color_3, @color_4]
-  # end
 end
