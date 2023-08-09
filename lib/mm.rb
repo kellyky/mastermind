0c89711e8f71866e699489558637e8557a13fe9e
Kelly Young <kelpopko@gmail.com>
2023-08-08 22:50:08 -0500

    Add first working version of game

--- lib/mm.rb
+++ lib/mm.rb
@@ -1,44 +1,35 @@
 require 'pry-byebug'
 
-# code for counting & tallying the guesses
-class IncrementGuess
-  def initialize(guess_count=0)
-    @guess_count = guess_count
-  end
 
-  def increment_turn_counter
-    @guess_count += 1
-  end
-end
-
-class CountTurns  # rename or break up this class; it's a bit hodgepodge
+class PlayMasterMind
   def initialize
     @encoded_colors = SetCode.all_colors
-    # @guessed_colors = GuessCode.new(a, b, c, d).all_colors
   end
   
+  def get_code_guess
+    @guessed_code = GuessCode.player_attempts_to_break_the_code
+  end
 
   def run_through_turns(turns_done=0)
     if turns_done <= 12
       guess_count = IncrementGuess.new(turns_done)
       next_turn = guess_count.increment_turn_counter
-      @guessed_code = GuessCode.player_attempts_to_break_the_code
+      get_code_guess
       assess_the_payers_attempt_to_break_the_code
       run_through_turns(next_turn)
     else
-      puts "12th turn!, game over" # placeholder for end game
+      EndGame.better_luck_next_time
     end
   end
 
   def assess_the_payers_attempt_to_break_the_code
-    best = 0
-    next_best = 0
+    best, next_best = [0, 0]
     rating = {}
-    for place in (0...4)      
-      if @guessed_code[place] == @encoded_colors[place]
+    for index in (0...4)
+      if @guessed_code[index] == @encoded_colors[index]
         best += 1
       elsif
-        @encoded_colors.any?(@guessed_code[place])
+        @encoded_colors.any?(@guessed_code[index])
         next_best += 1
       else
         next
@@ -46,13 +37,20 @@ class CountTurns  # rename or break up this class; it's a bit hodgepodge
     end
 
     rating = KeyPegs.new(best, next_best)
+    puts "you guessed #{@guessed_code}"
     score = { best: "#{rating.fully_correct_qty}", next_best: "#{rating.partly_correct_qty}" }
-    puts "you guessed #{@guessed_code}; code was #{@encoded_colors}" # temp
-    display_rating(score)
+    puts "your score: #{score}"
+    EndGame.we_have_a_winner if best == 4
+  end
+end
+
+class IncrementGuess
+  def initialize(guess_count=0)
+    @guess_count = guess_count
   end
 
-  def display_rating(rating)
-    puts rating.to_s
+  def increment_turn_counter
+    @guess_count += 1
   end
 end
 
@@ -75,7 +73,12 @@ class SetCode
   COLOR_BANK = [:red, :orange, :yellow, :green, :blue, :indigo, :violet]
 
   def self.all_colors
-    COLOR_BANK.shuffle.first(4)
+    colors = []
+    4.times do
+      colors << COLOR_BANK.shuffle.last
+    end
+    # colors
+    [:red, :red, :orange, :yellow]  # for testing. Otherwise uncomment colors and remove this line
   end
 end
 
@@ -87,10 +90,21 @@ class GuessCode
     until colors.count == 4
       puts "state your choice for color #{counter + 1}."
       color = gets.chomp
-      colors << color.to_sym
+      colors << color.strip.to_sym
       counter += 1
     end
     colors
   end
+end
 
+class EndGame
+  def self.we_have_a_winner
+    puts "whoohoo!!!"
+    exit
+  end
+
+  def self.better_luck_next_time
+    puts "Your 12 turns are up. Better luck next time!"
+    exit
+  end
 end
