22c3abfc0eb1d32b0fc352a9a4109cebe308565f
Kelly Popko <kelpopko@gmail.com>
2024-03-20 22:44:58 -0500

    Fix copy to accomodate game modes

--- lib/game_intro.rb
+++ lib/game_intro.rb
@@ -13,15 +13,13 @@ class GameIntro
   def self.rules
     PrettyDisplay.puts_pause(">>>==============================================================<<<", 2)
     PrettyDisplay.puts_pause("\n>>> Gameplay & Scoring:\n\n")
-    PrettyDisplay.animated_text("The code maker creates a 4-item code.\n\n")
-    PrettyDisplay.animated_text("To win the game, the code breaker must guess the code within 12 attempts.\n")
+    PrettyDisplay.animated_text("The code maker creates a code using colors.\n\n")
+    PrettyDisplay.animated_text("To win, guess the code within 12 attempts.\n")
     PrettyDisplay.animated_text("Each attempt is scored to give the code breaker clues.\n\n")
     PrettyDisplay.animated_text("Scoring works like this:\n")
     PrettyDisplay.animated_text("- Black point(s): 1 for each 'place' with the correct color\n")
     PrettyDisplay.animated_text("- White points(s): 1 for each 'place' whose color is in the code - but doesn't go in that 'place'\n")
     PrettyDisplay.animated_text("- No points: For 'places' with wrong color (and when that color is NOT in the code at all)\n\n")
-    PrettyDisplay.animated_text("The code has 4 'places', and each place has a color. Colors may repeat.\n\n")
-    PrettyDisplay.animated_text("You can repeat colors if you'd like, but be sure to choose colors the computer knows. \n")
-    PrettyDisplay.animated_text("You can type just the first 3 letters of the color - I'll understand! :D\n\n")
+    PrettyDisplay.animated_text("Each 'place' in the code is a color. Colors may repeat.\n\n")
   end
 end
