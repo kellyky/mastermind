092991bb2813281667fbaf73c954c2c684446f4f
Kelly Popko <kelpopko@gmail.com>
2024-03-25 18:21:30 -0700

    Refactor SpyRole

--- lib/spy_role.rb
+++ lib/spy_role.rb
@@ -13,7 +13,7 @@ class SpyRole
   ROLES = {
     '1' => :computer,
     '2' => :player
-  }
+  }.freeze
 
   def initialize
     @code_breaker = :player
@@ -22,9 +22,9 @@ class SpyRole
 
   def assign_roles
     begin
-      self.send("#{ROLES[player_answer]}_code_breaker")
-    rescue NoMethodError => e
-      PrettyDisplay.puts_pause("\nHm, I don't know that one. Please choose '1' to set the code or '2' to break the code.", 2)
+      send("#{ROLES[player_answer]}_code_breaker")
+    rescue NoMethodError
+      PrettyDisplay.puts_pause("\nPlease choose '1' to set the code or '2' to break the code.", 2)
       retry
     end
 
@@ -36,12 +36,13 @@ class SpyRole
   end
 
   def computer_code_breaker
-    PrettyDisplay.animated_text("\ncode maker it is! First, you'll be prompted to create a 4-item code, 1 color at a time. \nThen, the computer will try to break it.\n\n")
+    PrettyDisplay.animated_text("\nCode maker it is!\m")
+    PrettyDisplay.animated_text("\n\nYou'll CREATE the code then the computer will try to break it.\n\n")
     @code_maker = :player
     @code_breaker = :computer
   end
 
   def player_code_breaker
-    PrettyDisplay.puts_pause("\ncode breaker it is! The computer has created a 4-'place' code. \nYOUR goal is to crack a 4-color code set by the computer.", 2)
+    PrettyDisplay.puts_pause("\n\nThe computer creates the code - YOU need to break the code.", 2)
   end
 end
