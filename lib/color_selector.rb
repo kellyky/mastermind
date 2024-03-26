806ac19fc7296086fccc9ebf44f4141717628602
Kelly Popko <kelpopko@gmail.com>
2024-03-25 21:02:19 -0700

    Refactor ColorSelector to simplify

--- lib/color_selector.rb
+++ lib/color_selector.rb
@@ -7,14 +7,15 @@ require 'pry-byebug'
 
 # Logic for encoding / decoding color
 class ColorSelector
-  
-  def initialize(code_maker, code_breaker, difficulty=:standard, code_length=4, guesses_left=12)
+  attr_reader :guesses_left
+
+  def initialize(code_maker, code_breaker, difficulty = :standard, code_length = 4, guesses_left = 12)
     @@difficulty = difficulty
     @@code_length = code_length
     @guesses_left = guesses_left
     @@code_maker = code_maker
     @@code_breaker = code_breaker
-    @@color_bank = [:red, :orange, :yellow, :green, :blue, :indigo, :violet]
+    @@color_bank = %i[red orange yellow green blue indigo violet]
   end
 
   def color_bank
@@ -28,125 +29,69 @@ class ColorSelector
     end
   end
 
-  def guesses_left
-    @guesses_left
-  end
-
   def code_length
     @@code_length
   end
 
   def difficulty
-    difficulty
+    @@difficulty
   end
 
-  def get_code
+  def encode_colors
     case @@code_maker
-    when :computer
-      Computer.new(guesses_left).set_code
-    when :player
-      Player.new(guesses_left).set_code
+    when :computer then computer_select_colors
+    when :player then Player.new(guesses_left).select_colors
     end
   end
 
   def break_code(guesses_left)
     case @@code_breaker
-    when :computer
-      Computer.new(guesses_left).break_code
-    when :player
-      Player.new(guesses_left).break_code
+    when :computer then computer_select_colors
+    when :player then Player.new(guesses_left).select_colors
     end
   end
 
   def show_color_bank
     print "\nColors to choose from: \n>> | "
-    color_bank.each do |color|
-      PrettyDisplay.color_text(color)
-    end
+    color_bank.each { |color| PrettyDisplay.color_text(color) }
     print " <<\n\n\n"
   end
 
-  class Computer < ColorSelector
-    def initialize(guesses_left)
-      @guesses_left = guesses_left
-    end
-
-    def guesses_left
-      super
-    end
-
-    def color_bank
-      super
-    end
-
-    def code_length
-      super
-    end
-
-    def set_code
-      select_colors
-    end
-
-    def break_code
-      show_color_bank
-      select_colors
-    end
-
-    def select_colors
-      selected_colors = []
-      code_length.times do
-        selected_colors << color_bank.sample
-      end
-      selected_colors
+  def print_guesses_left
+    if guesses_left.zero?
+      'This is your last guess. Make it count!'
+    else
+      "You have #{guesses_left} guesses left."
     end
   end
 
-  # Logic to support player encoding colors & validating player guesses
+  def computer_select_colors
+    show_color_bank
+    selected_colors = []
+    code_length.times { selected_colors << color_bank.sample }
+    selected_colors
+  end
+
+  # Logic to support player-selected colors whether setting or guessing the code
   class Player < ColorSelector
+    COLORS = {
+      red: :red,
+      ora: :orange,
+      yel: :yellow,
+      gre: :green,
+      blu: :blue,
+      ind: :indigo,
+      vio: :violet
+    }.freeze
+
     def initialize(guesses_left)
       @guesses_left = guesses_left
       @selected_colors = []
       @color_counter = 0
     end
 
-    def set_code
-      Player.new(guesses_left).select_colors
-    end
-
-    def color_bank
-      super
-    end
-
-    def code_length
-      super
-    end
-
-    def show_color_bank
-      super
-    end
-
-    def guesses_left
-      @guesses_left
-    end
-
-    def print_guesses_left
-      if guesses_left.zero?
-        'This is your last guess. Make it count!'
-      else
-        "You have #{guesses_left} guesses left."
-      end
-    end
-
-    def round
-      12 - guesses_left
-    end
-
-    def break_code
-      PrettyDisplay.puts_pause("*** Round #{round} - #{print_guesses_left}")
-      Player.new(guesses_left).select_colors
-    end
-
     def select_colors
+      PrettyDisplay.puts_pause("*** Round #{12 - guesses_left} - #{print_guesses_left}")
       show_color_bank
       until @selected_colors.count == code_length
         select_color
@@ -155,41 +100,18 @@ class ColorSelector
       @selected_colors
     end
 
-    def select_color(color = '')
-      tell_user_to_select_color
+    def select_color
+      print "#{PrettyDisplay.animated_elipses}select a color for 'place' #{@color_counter + 1}: \n\n"
+
       color = gets.chomp
-      puts ''
-      formatted_color = format_color(color)
-      if validate_color?(formatted_color)
-        @selected_colors << get_full_color_name(formatted_color)
-      else
+      formatted_color = color.strip.downcase.slice(0..2).to_sym
+
+      if COLORS[formatted_color].nil?
         PrettyDisplay.puts_pause("Hm, I don't know that one. Please choose an available color.")
-        select_color(color)
+        select_color
+      else
+        @selected_colors << COLORS[formatted_color]
       end
-    end
-
-    def get_full_color_name(color)
-      color_bank.each do |bank_color|
-        if bank_color.match?(color.slice(0..2)) || bank_color.to_s.chars.sort == color.to_s.chars.sort
-          return bank_color
-        end
-      end
-    end
-
-    def validate_color?(color)
-      color_bank.any? do |bank_color|
-        if color.length >= 3
-          bank_color.match?(color.slice(0..2)) || bank_color.to_s.chars.sort == color.to_s.chars.sort
-        end
-      end
-    end
-
-    def tell_user_to_select_color
-      print "#{PrettyDisplay.animated_elipses}select a color for 'place' #{@color_counter + 1}: "
-    end
-
-    def format_color(color)
-      color.strip.downcase.to_sym
     end
   end
 end
