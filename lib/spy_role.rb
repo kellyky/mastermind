6b3e5ac9164470ec7edcccba11b99ff8673fb836
Kelly Popko <kelpopko@gmail.com>
2024-06-22 22:56:17 -0500

    Refactor (small) SpyRole for readability and add test coverage

--- lib/spy_role.rb
+++ lib/spy_role.rb
@@ -25,7 +25,9 @@ class SpyRole
     begin
       send("#{ROLES[player_answer]}_code_breaker")
     rescue NoMethodError
-      PrettyDisplay.puts_pause("\nPlease choose '1' to set the code or '2' to break the code.", 2)
+      PrettyDisplay.puts_pause(
+        "\nPlease choose '1' to set the code or '2' to break the code.", 2
+        )
       retry
     end
 
@@ -39,13 +41,17 @@ class SpyRole
   # Sets computer as code breacker and player as code maker
   def computer_code_breaker
     PrettyDisplay.animated_text("\nCode maker it is!\m")
-    PrettyDisplay.animated_text("\n\nYou'll CREATE the code then the computer will try to break it.\n\n")
+    PrettyDisplay.animated_text(
+      "\n\nYou'll CREATE the code then the computer will try to break it.\n\n"
+      )
     @code_maker = :player
     @code_breaker = :computer
   end
 
   # Confirms human as code maker and computer as code breaker
   def player_code_breaker
-    PrettyDisplay.puts_pause("\n\nThe computer creates the code - YOU need to break the code.", 2)
+    PrettyDisplay.puts_pause(
+      "\n\nThe computer creates the code - YOU need to break the code.", 2
+      )
   end
 end
--- spec/spy_role_spec.rb
+++ spec/spy_role_spec.rb
@@ -22,6 +22,12 @@ describe SpyRole do
         expect(subject).to receive(:computer_code_breaker)
         subject.assign_roles
       end
+
+      it 'assigns the correct roles' do
+        subject.assign_roles
+        expect(subject.code_maker).to be(:player)
+        expect(subject.code_breaker).to be(:computer)
+      end
     end
 
     context 'when "2" is selected' do
@@ -31,16 +37,30 @@ describe SpyRole do
         expect(subject).to receive(:player_code_breaker)
         subject.assign_roles
       end
+
+      it 'assigns the correct roles' do
+        subject.assign_roles
+        expect(subject.code_maker).to be(:computer)
+        expect(subject.code_breaker).to be(:player)
+      end
     end
 
-    # FIXME: - not sure how to test the error & rescue withuot endless loop
-    context 'when anything else is selected' do
-      # it 'raises StandardError and retries' do
-      #   allow(subject).to receive(:player_answer).and_return('5')
-      #   expect(subject).to receive(:assign_roles).once.and_raise
-      #   expect(subject).to receive(:assign_roles).once.and_call_original
-      #   subject.assign_roles
-      # end
+    context 'when invalid input is selected (initially)' do
+      before do
+        # Stubbing first an invalid user input followed by '1' - to set the computer as code breaker
+        allow(subject).to receive(:player_answer).and_return('g', '1')
+      end
+      it 'raises StandardError and retries' do
+        error_message = "\nPlease choose '1' to set the code or '2' to break the code."
+
+        # Expecting the method to retry and eventually call computer_code_breaker after the invalid input
+        expect(subject).to receive(:computer_code_breaker)
+
+        # Expecting PrettyDisplay.puts_pause to be called with the error message once
+        expect(PrettyDisplay).to receive(:puts_pause).with(error_message, 2).once
+
+        subject.assign_roles
+      end
     end
   end
 
